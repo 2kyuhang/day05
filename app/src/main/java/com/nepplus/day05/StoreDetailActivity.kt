44d58a4a24f73dc6cba695239d4808a5ffd53bc9
이규행 <2kyuhang@naver.com>
2022-07-29 15:49:37 +0900

    피자

---
+++ app/src/main/java/com/nepplus/day05/StoreDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.day05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activty_store_detail)
+    }
+}
