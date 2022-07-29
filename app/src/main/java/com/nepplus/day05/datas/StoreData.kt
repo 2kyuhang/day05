510338f13350f0330aa9b0135269b4ea6051c83b
이규행 <2kyuhang@naver.com>
2022-07-29 15:55:52 +0900

    피자

--- app/src/main/java/com/nepplus/day05/datas/StoreData.kt
+++ app/src/main/java/com/nepplus/day05/datas/StoreData.kt
@@ -1,10 +1,14 @@
 package com.nepplus.day05.datas
 
+import java.io.Serializable
+
 class StoreData (
     val title : String,
     val score : Double,
+    val phoneNum : String,
     val imgUrl : String
-        ){
+
+        ):Serializable{
 
 
 
--- app/src/main/java/com/nepplus/day05/fragment/PizzaFragment.kt
+++ app/src/main/java/com/nepplus/day05/fragment/PizzaFragment.kt
@@ -1,11 +1,13 @@
 package com.nepplus.day05.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.nepplus.day05.R
+import com.nepplus.day05.StoreDetailActivity
 import com.nepplus.day05.adapters.StoreListViewAdapter
 import com.nepplus.day05.datas.StoreData
 import kotlinx.android.synthetic.main.fragment_pizza.*
@@ -30,14 +32,23 @@ class PizzaFragment: Fragment(){
         super.onActivityCreated(savedInstanceState)
 
         //더미데이터 //실제로는 여기에서 서버 데이터 받아온다
-        mPizzaList.add(StoreData("피자헛", 4.3,"https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
-        mPizzaList.add(StoreData("파파존스", 3.8,"https://mblogthumb-phinf.pstatic.net/20160530_116/ppanppane_14646177654870uGy9_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%281%29.png?type=w800"))
-        mPizzaList.add(StoreData("도미노피자", 4.5,"https://www.pngplay.com/wp-content/uploads/6/Dominos-Pizza-Logo-Background-PNG-Image.png"))
-        mPizzaList.add(StoreData("미스터피자", 3.2,"https://mblogthumb-phinf.pstatic.net/20160530_62/ppanppane_14646177047843NP3E_PNG/%B9%CC%BD%BA%C5%CD%C7%C7%C0%DA_%B7%CE%B0%ED_%283%29.png?type=w800"))
+        mPizzaList.add(StoreData("피자헛", 4.3,"1577-8080","https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
+        mPizzaList.add(StoreData("파파존스", 3.8,"1577-8080","https://mblogthumb-phinf.pstatic.net/20160530_116/ppanppane_14646177654870uGy9_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%281%29.png?type=w800"))
+        mPizzaList.add(StoreData("도미노피자", 4.5,"1577-3082","https://www.pngplay.com/wp-content/uploads/6/Dominos-Pizza-Logo-Background-PNG-Image.png"))
+        mPizzaList.add(StoreData("미스터피자", 3.2,"1577-0077","https://mblogthumb-phinf.pstatic.net/20160530_62/ppanppane_14646177047843NP3E_PNG/%B9%CC%BD%BA%C5%CD%C7%C7%C0%DA_%B7%CE%B0%ED_%283%29.png?type=w800"))
 
         mPizzAdapter = StoreListViewAdapter(requireContext(), R.layout.store_list_item, mPizzaList)
 
         pizzaListView.adapter = mPizzAdapter
+
+        pizzaListView.setOnItemClickListener { adapterView, view, i, l ->
+            val myIntent = Intent(requireContext(), StoreDetailActivity::class.java)
+
+            myIntent.putExtra("storeData", mPizzaList[i])
+
+            startActivity(myIntent)
+        }
+
 
     }
 
